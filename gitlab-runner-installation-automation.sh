96d75c27e05f1b2a901aa00fec3a5f9ae564e044
Lucca Tisser Paradeda <92058802+Luccatp@users.noreply.github.com>
2023-10-24 19:44:40 -0300

    Create gitlab-runner-installation-automation.sh
    Creating a shell command that creates and sets up a gitlab-runner runner

---
+++ gitlab-runner-installation-automation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+url=$1
+token=$2
+
+sudo su
+yum -y install docker
+
+systemctl start docker | true
+systemctl enable docker
+
+containerId=$(sudo docker run -d gitlab/gitlab-runner)
+
+docker exec -it $containerId /bin/bash
+
+apt update
+
+apt install -y vim
+
+apt install gitlab-runner
+
+gitlab-runner register --non-interactive --url $url --registration-token $token --executor shell
